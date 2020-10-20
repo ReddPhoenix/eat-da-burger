2d972451b13c0b4a63fc2767ca09c5aebd9febc7
ReddPhoenix <wsguerrero@gmail.com>
2020-10-20 13:45:44 -0600

    created models folder & burger.js

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+// Import orm.js into burger.js
+const orm = require('../config/orm');
+
+// Create the code that will call the ORM functions 
+// using burger specific input for the ORM.
+
+const burger = {
+    // selectAll Function
+    // How to use functions?
+    
+    // insertOne Function
+    // How to use functions?
+
+    // updateOne Function
+    // How to use functions?
+
+    }
+
+// Export in module.exports
+module.exports = burger;
