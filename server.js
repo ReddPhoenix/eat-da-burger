95b51fd3f1b3ab6278dd281023ea5a04020ccb50
ReddPhoenix <wsguerrero@gmail.com>
2020-10-21 13:59:37 -0600

    initialized express handlebars files & routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// Module requirement for Express
+// Module requirements for Express
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 // Express assigned to app variable
 const app = express();
 
 // Variable assigned to PORT for server
 const PORT = process.env.PORT || 3000;
+
+// Initialize & setup handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 //  Added listener for assigned PORT for server
 app.listen(PORT, () => {
