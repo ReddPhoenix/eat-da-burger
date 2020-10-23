d1b01739c48809eb9c3d057833aed681d2afb187
ReddPhoenix <wsguerrero@gmail.com>
2020-10-22 22:53:45 -0600

    updated handlebars body & port for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,23 @@ const mysql = require("mysql");
 
 require('dotenv').config();
 
-// Connection to mysql
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "roster_db",
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    // use jaws db
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // use local connection
+    // Connection to mysql
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "roster_db",
+    });
+}
+
 
 connection.connect(function (error) {
     if (error) throw error;
